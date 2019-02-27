4df5821500ee042f5c76c4296ce65a1ded82bcfc
Sebastian Szwaczyk <sszwaczyk@gmail.com>
2019-02-27 11:36:30 +0100

    Downloading and deleting file

--- src/main/java/pl/sszwaczyk/domain/generator/RequestsGenerator.java
+++ src/main/java/pl/sszwaczyk/domain/generator/RequestsGenerator.java
@@ -10,8 +10,15 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 import java.util.Random;
+import java.util.UUID;
 
 @Slf4j
 public abstract class RequestsGenerator {
@@ -39,25 +46,43 @@ public abstract class RequestsGenerator {
                 log.info("Sending request to service " + service.getId() + " for path " + service.getPath());
                 long start = System.currentTimeMillis();
                 Response response = request.get();
-                long timeOfRealization = System.currentTimeMillis() - start;
                 log.info("Response status = " + response.getStatus());
                 if(response.getStatus() >= 200 && response.getStatus() < 300) {
+                    Path path = downloadFile(response);
+                    long timeOfRealization = System.currentTimeMillis() - start;
+                    deleteFile(path);
                     Statistics.getInstance().updateSuccess(service, timeOfRealization);
                     log.info("Request for service " + service.getId() + " completed successfully in " + timeOfRealization + " ms.");
-                    log.info("Response size is " + response.getHeaders().get("Content-Length") + " Bytes");
                 } else {
                     Statistics.getInstance().updateFailed(service);
                     log.info("Request for service " + service.getId() + " failed");
                 }
             } catch (Exception ex) {
                 Statistics.getInstance().updateFailed(service);
-                log.info("Request for service " + service.getId() + " failed because of " + ex.getMessage());
+                log.error("Request for service " + service.getId() + " failed because of " + ex.getMessage());
             }
 
             int gap = getNextGapInSeconds();
             log.info("Generating next request in " + gap + " seconds");
             Thread.sleep(gap * 1000);
+
         }
+    }
+
+    private void deleteFile(Path path) throws IOException {
+        log.info("Deleting file " + path.toAbsolutePath() + "...");
+        Files.delete(path);
+        log.info("File deleted");
+    }
+
+    private Path downloadFile(Response response) throws IOException {
+        Path path = Paths.get("/tmp/" + UUID.randomUUID());
+        InputStream inputStream = response.readEntity(InputStream.class);
+        log.info("Downloading to " + path.toAbsolutePath() + "...");
+        Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
+        inputStream.close();
+        log.info("File downloaded");
+        return path;
     }
 
     public abstract int getNextGapInSeconds();
